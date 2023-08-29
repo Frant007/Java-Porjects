2dbf09149a3152c4303ac99ed9113c832f01a627
Francisco Chahla <114193407+Frant007@users.noreply.github.com>
2023-08-29 20:23:50 -0300

    Delete ToDo API directory

--- ToDo API/src/main/java/com/fran/app/rest/Repository/ToDoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fran.app.rest.Repository;
-
-import com.fran.app.rest.Model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ToDoRepository extends JpaRepository<Task, Long> {
-}
